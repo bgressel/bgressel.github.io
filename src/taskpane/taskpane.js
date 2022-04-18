f3ffb2603c537654d58e74d6ec5b02d37a422e1e
bgressel <54748739+bgressel@users.noreply.github.com>
2022-04-18 17:13:31 -0400

    Add files via upload

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,23 @@
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Word) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+async function run() {
+  return Word.run(async (context) => {
+    /**
+     * Insert your Word code here
+     */
+
+    // insert a paragraph at the end of the document.
+    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+
+    // change the paragraph color to blue.
+    paragraph.font.color = "blue";
+
+    await context.sync();
+  });
+}
